a8af164f5ab53816413229552bcfa542b228f9c5
Muskan <muskanpanday31@gmail.com>
2025-11-02 15:37:39 +0530

    Initial commit

---
+++ InvertedAndRotatedHalfPyramid.java
@@ -0,0 +1,29 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class InvertedAndRotatedHalfPyramid {
+    public static void invertedRotatedHalfPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        invertedRotatedHalfPyramid(n);
+
+    }
+}
